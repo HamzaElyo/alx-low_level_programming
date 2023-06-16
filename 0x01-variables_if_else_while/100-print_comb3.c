c9f637a42931bd509fe98e51f278d5c12fad06d8
HamzaElyo <elyoussoufihamza@gmail.com>
2023-06-16 16:08:07 -0700

    combo3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,10 +9,10 @@ int main(void)
 int x, y;
 for (x = '0' ; x <= '8' ; x++)
 {
-for (y = x+1 ; y <= '9'; y++) 
+for (y = x + 1 ; y <= '9'; y++)
 {
 putchar(x);
- putchar(y);
+putchar(y);
 if ( x != '8' || y != '9')
 {
 putchar(',');
