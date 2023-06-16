d585ffc12c1df606cfc45ac132c2c8b33fb35a96
HamzaElyo <elyoussoufihamza@gmail.com>
2023-06-16 12:58:39 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - write all alphabtes
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+for (x = '0' ; x <= '9' ; x++)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
