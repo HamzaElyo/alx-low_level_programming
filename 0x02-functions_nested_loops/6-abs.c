0d29ce5736b26dd84c759d3c0ed0a659cc3ca408
HamzaElyo <elyoussoufihamza@gmail.com>
2023-06-20 16:56:05 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value 
+ * @n: input number
+ * Return: absolute value.
+ */
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+else
+return (n * -1);
+_putchar('\n');
+}
