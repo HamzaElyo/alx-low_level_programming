10482e27d2ae375700b0a5bd72d69d13ae6c4ea4
HamzaElyo <elyoussoufihamza@gmail.com>
2023-06-20 16:20:01 -0700

    betty styles

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _islower - checks if a letter is lower case
+ * islower - checks if a letter is lower case
  * Return: Always 0
  */
 int _islower(int c)
 {
-if ( c > 96 && c < 123 )
+if (c > 96 && c < 123)
 return (1);
 else
 return (0);
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _isalpha - checks if is a letter
+ * isalpha - checks if is a letter
  * Return 
  */
 int _isalpha(int c)
 {
-  if ( (c > 96 && c < 123) || (c > 64 && c < 91) )
+if ((c > 96 && c < 123) || (c > 64 && c < 91))
 return (1);
 else
 return (0);
