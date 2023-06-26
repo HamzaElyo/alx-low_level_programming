de880ce4f7377cd7bf4616b1d99f577576ca47bb
HamzaElyo <elyoussoufihamza@gmail.com>
2023-06-26 07:05:39 -0700

    put

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include <main.h>
+/**
+ * _puts - a fonction that orints a string followed by a new line
+ * @str:the string to be printed
+ * Return: void
+ */
+void _puts(char *str)
+{
+while (*str)
+_putchar(*str++);
+_putchar('\n');
+}
