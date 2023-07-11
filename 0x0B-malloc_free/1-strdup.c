72c26eaee553f969c946e76fd451d20ebb626737
HamzaElyo <elyoussoufihamza@gmail.com>
2023-07-11 05:57:57 -0700

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * strdup - function that returns a pointer
- * @str - string copied
+ * _strdup - function that returns a pointer
+ * @str:cstring copied
  * Return: a string
  */
 char *_strdup(char *str)
@@ -15,7 +15,7 @@ for (i = 0; str[i] != '\0'; i++)
 {
 j = i + 1;
 }
-s = malloc(j + 1);
+s = malloc(i + 1);
 if (s == NULL)
 return (NULL);
 for (i = 0; i < j ; i++)
